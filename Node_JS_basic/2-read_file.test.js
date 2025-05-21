258645daf13e43a067fe91431b43579513d53f93
Rogergrosado <rgrosado1@gmail.com>
2025-05-21 13:24:56 -0400

    fixing 2-read file

--- Node_JS_basic/2-read_file.test.js
+++ Node_JS_basic/2-read_file.test.js
@@ -5,9 +5,19 @@ const { execSync } = require('child_process');
 describe('countStudents', () => {
   it('prints the correct output for database.csv', () => {
     const output = execSync('node 2-main_1.js').toString().trim();
+
     assert.ok(output.includes('Number of students: 10'));
     assert.ok(output.includes('Number of students in CS: 6'));
     assert.ok(output.includes('Number of students in SWE: 4'));
+  });
+
+  it('throws an error when file is missing', () => {
+    try {
+      execSync('node 2-main_0.js');
+      throw new Error('Expected error was not thrown');
+    } catch (error) {
+      assert.ok(error.stderr.toString().includes('Cannot load the database'));
+    }
   });
 });
 
